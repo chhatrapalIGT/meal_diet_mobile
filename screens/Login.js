b01d21ac70498e26ee454ff1c017e5b737ff9720
Sanjay <sanjay.igenerate@gmail.com>
2024-04-25 18:22:10 +0530

    Fixed minor issue

--- screens/Login.js
+++ screens/Login.js
@@ -10,8 +10,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Login = ({ navigation }) => {
   const [isPasswordShown, setIsPasswordShown] = useState(false);
-  const [email, setEmail] = useState('kajal1@yopmail.com');
-  const [password, setPassword] = useState('kajal@1234');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const showToast = (type, text1, text2) => {
     Toast.show({
